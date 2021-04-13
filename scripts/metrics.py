245e73d02e0660fb95e5dff8c6c0717e901daddd
Sayak Paul <spsayakpaul@gmail.com>
2021-04-13 18:28:43 +0530

    Update metrics.py

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -2,24 +2,24 @@ from . import calibration_tools
 import tensorflow as tf
 
 
-def get_confidence(loader, model, mask, type="odin"):
+def get_confidence(loader, model, type="odin"):
     if type == "odin":
-        logits = model.predict(loader)[:, mask]
+        logits = model.predict(loader)
         confidence = tf.nn.softmax(logits, axis=1)
         confidence = tf.reduce_max(confidence, 1)
     else:
-        confidence = model.predict(loader)[:, mask]
+        confidence = model.predict(loader)
         confidence = tf.reduce_max(confidence, 1)
     return confidence.numpy()
 
 
-def calculate_auroc(in_loader, out_loader, model, mask, type):
+def calculate_auroc(in_loader, out_loader, model, type):
     confidence_in = get_confidence(
-        in_loader, model=model, mask=mask, type=type
+        in_loader, model=model, type=type
     )
     in_score = -confidence_in
     confidence_out = get_confidence(
-        out_loader, model=model, mask=mask, type=type
+        out_loader, model=model, type=type
     )
     out_score = -confidence_out
 
